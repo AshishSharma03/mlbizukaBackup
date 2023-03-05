8ba2d09b94a3faef896739c642770d1b1145a7a3
AshishSharma03 <30octaksharma@gmail.com>
2023-03-05 22:51:02 +0530

    check_

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require("http");
 const PORT = 5000
 
-app.use('/',express.static('public'))
-console.log("ss")
+app.use('/ss',express.static('public'))
+app.use('/',(req,res)=>{
+    res.json("connect")
+})
 
 app.listen(PORT,()=>{
     console.log("Server listen on "+ PORT)
